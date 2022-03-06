3ebc3f358e96308793ddff54223cfebb630d7e9e
serega-s <comp.it.05@inbox.ru>
2022-03-06 13:36:59 +0300

    added frontpage with products

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
+from product.models import Product
 
 
-class FrontPageView(generic.TemplateView):
+class FrontPageView(generic.ListView):
+    queryset = Product.objects.all()[0:8]
+    context_object_name = 'products'
     template_name = 'core/frontpage.html'
