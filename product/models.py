daa79cea7066b2d652153e8ce0823a7457289844
serega-s <comp.it.05@inbox.ru>
2022-03-06 13:14:05 +0300

    added model Category

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return self.name
