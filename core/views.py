917acbaa47e54c93d639cc4b8d25539fa91c8943
serega-s <comp.it.05@inbox.ru>
2022-04-10 21:44:01 +0300

    deleted unused imports

--- core/views.py
+++ core/views.py
@@ -1,18 +1,10 @@
-from django.contrib.auth import login
-from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView
-from django.db.models import Count, Q
-from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
-from django.views import generic, View
-from django.views.generic.base import TemplateResponseMixin
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.views import generic
 
 from product.models import Category, Product
 
-from .forms import SignUpForm
+User = get_user_model()
 
 
 class FrontPageView(generic.ListView):
@@ -52,45 +44,3 @@ class ShopView(generic.ListView):
                 Q(name__icontains=query) | Q(description__icontains=query))
 
         return queryset
-
-
-class SignUpView(generic.CreateView):
-    form_class = SignUpForm
-    template_name = 'core/signup.html'
-    success_url = reverse_lazy('login')
-
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-
-        return redirect('frontpage')
-
-
-class LogInView(LoginView):
-    form = AuthenticationForm
-    template_name = 'core/login.html'
-
-    def get_success_url(self):
-        return reverse_lazy('frontpage')
-
-
-class LogOutView(generic.RedirectView):
-    url = '/login/'
-
-    def get(self, request, *args, **kwargs):
-        auth_logout(request)
-        return super(LogOutView, self).get(request, *args, **kwargs)
-
-
-class MyAccountView(LoginRequiredMixin, generic.TemplateView):
-    template_name = 'core/myaccount.html'
-
-
-class EditMyAccountView(LoginRequiredMixin, generic.UpdateView):
-    model = User
-    template_name = 'core/edit_myaccount.html'
-    fields = ['first_name', 'last_name', 'username', 'email']
-    success_url = reverse_lazy('myaccount')
-
-    def get_object(self, queryset=None):
-        return self.request.user
