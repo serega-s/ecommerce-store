04d88d56e65bd646b11a378502e8cace350d0a40
serega-s <comp.it.05@inbox.ru>
2022-04-20 16:47:13 +0300

    added possibility to add multiple addresses and edit them

--- order/urls.py
+++ order/urls.py
@@ -4,4 +4,7 @@ from order import views
 
 urlpatterns = [
     path('start_order/', views.StartOrderView.as_view(), name='start_order'),
+    path('add_address/', views.AddAddressView.as_view(), name='add_address'),
+    path('edit_address/<int:pk>/', views.EditAddressView.as_view(), name='edit_address'),
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
 ]
