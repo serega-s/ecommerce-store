477b236cdd9440dd9060866f6e63face095caa81
serega-s <comp.it.05@inbox.ru>
2022-04-29 22:23:43 +0300

    updated admin displays

--- users/admin.py
+++ users/admin.py
@@ -3,23 +3,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-admin.site.register(User)
-# @admin.register(CustomUser)
-# class CustomUserAdmin(UserAdmin):
-#     add_form = CustomUserCreationForm
-#     form = CustomUserChangeForm
-#     model = CustomUser
-#     list_display = ['email', 'first_name', 'last_name', 'is_staff', 'is_active']
-#     list_filter = ['email', 'first_name', 'last_name', 'is_staff', 'is_active']
-#     # fieldsets = (
-#     #     (None, {'fields': ('email', 'password')}),
-#     #     ('Permissions', {'fields': ('is_staff', 'is_active')}),
-#     # )
-#     # add_fieldsets = (
-#     #     (None, {
-#     #         'classes': ('wide',),
-#     #         'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
-#     #      ),
-#     # )
-#     search_fields = ['email']
-#     ordering = ['email']
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'is_active']
+    search_fields = ['email']
+    list_editable = ['is_active']
