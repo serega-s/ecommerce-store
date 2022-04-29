307b279f0561ed8d7f8021bdd8fce92556028c9d
serega-s <comp.it.05@inbox.ru>
2022-04-29 21:57:56 +0300

    improved admin filters and display

--- order/admin.py
+++ order/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from order.models import Order, ShippingAddress, OrderItem
 
-admin.site.register(Order)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "status", "created_at"]
+    list_filter = ["status", "created_at"]
+    search_fields = ["address__first_name", "address__address", "address__place"]
+    inlines = [OrderItemInline]
+
+
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
