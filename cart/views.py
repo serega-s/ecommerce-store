2661394c7378125d123586d171541e327510cb10
serega-s <comp.it.05@inbox.ru>
2022-04-30 14:33:13 +0300

    refactoring

--- cart/views.py
+++ cart/views.py
@@ -5,15 +5,6 @@ from product.models import Product
 from .cart import Cart
 
 
-#
-# def add_to_cart(request, product_id):
-#     cart = Cart(request)
-#     product = get_object_or_404(Product, id=product_id)
-#     cart.add(product, update_quantity=True)
-#
-#     return render(request, 'cart/menu_cart.html')
-
-
 class CartView(generic.TemplateView):
     template_name = 'cart/cart.html'
 
